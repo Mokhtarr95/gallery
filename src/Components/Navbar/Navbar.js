de9ab5cefb6c8185be44498ef04b722d6e70290c
Mohamed Mokhtar <Mohamedmokhtarr95@gmail.com>
2021-06-15 23:31:41 +0200

    Navbar Component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import Search from "../Search/Search";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} size="1.3rem" />
+        </Link>
+        <ul className="navbar-ul">
+          <li className="home">
+            <Link to="/">Home</Link>
+          </li>
+          <Search />
+          <li className="saves">
+            <a href="#">My Saves</a>
+          </li>
+        </ul>
+      </nav>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose size="1.5rem" />
+            </Link>
+          </li>
+          <li className="nav-text">
+            <Link to="/fashion">
+              <span>Fashion</span>
+            </Link>
+          </li>
+          <li className="nav-text">
+            <Link to="/food">
+              <span>Food</span>
+            </Link>
+          </li>
+          <li className="nav-text">
+            <Link to="/nature">
+              <span>Nature</span>
+            </Link>
+          </li>
+          <li className="nav-text">
+            <Link to="/portrait">
+              <span>Portrait</span>
+            </Link>
+          </li>
+          <li className="nav-text">
+            <Link to="/wedding">
+              <span>Wedding</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
